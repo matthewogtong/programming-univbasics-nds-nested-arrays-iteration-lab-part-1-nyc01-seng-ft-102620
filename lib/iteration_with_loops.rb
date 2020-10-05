cf11f232ccb1ff24817be87bd528d5da7bc13d92
Matthew Ogtong <ogtongm@gmail.com>
2020-10-05 23:48:05 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -15,7 +15,11 @@ def find_even_values(src)
 
     while element_index < src[row_index].count
 
-      p src[row_index][element_index]
+      if src[row_index][element_index].even?
+
+        p src[row_index][element_index]
+
+      end
 
       element_index += 1
 
